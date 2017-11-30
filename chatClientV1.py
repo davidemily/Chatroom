b15008c5d602dd23e92f9b33105a5c7867c8c9cd
davidemily <daeth5@mail.missouri.edu>
2017-11-29 19:23:26 -0600

    latest changes

--- chatClientV1.py
+++ chatClientV1.py
@@ -6,6 +6,7 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 10528
 connections = 0
+loginFlag = 0
 
 host = input("What is the host name? ")
 
@@ -27,23 +28,8 @@ print("Type 'logout' to logout")
 print("Type 'menu' to print this menu again")
 print("")
 
-while 1:
-
-    message = input(>>)
-
-    if(message.split(' ',1)[0] == "logout"):
-        print("Good bye...")
-        sock.close()
-        break;
-
-    if(message.split(' ',1)[0] == "menu"):
-        print("")
-        print("Type 'login' to login")
-        print("Type 'newuser username password' to create a newuser")
-        print("Type 'send' to send a message")
-        print("Type 'logout' to logout")
-        print("Type 'menu' to print this menu again")
-        print("")
-
-    if(message.split(' ',1)[0] == "login"):
-  
+while (loginFlag == 0):
+    message = input(">>")
+    sock.send(message.encode())
+    message = sock.recv(1024).decode()
+    print(message)
--- chatServerV1.py
+++ chatServerV1.py
@@ -8,14 +8,27 @@ port = 10528
 
 sock.bind((host,port))
 sock.listen(3)
-print("#################################################")
+print("#####################################################################")
 print ("Chat server started on host: " +host+ " on port: " + str(port))
-print("#################################################")
+print("#####################################################################")
 print("")
 print ("Waiting for connection")
 
-addr = sock.accept()
+conn, addr = sock.accept()
 
-print("Client " +str(addr[0])+ "has connected!")
+print("")
+print("Client " + str(addr[0]) + "has connected!")
+print("")
 
-input()
+while True:
+    data = conn.recv(1024).decode()
+    if not data:
+        break
+    if(data.split(" ",1)[0] == "login"):
+        f = open ('login.txt', 'w')
+        print(f.read())
+    else:
+        print ("received: " + data)
+        conn.send(data.encode())
+
+conn.close()
